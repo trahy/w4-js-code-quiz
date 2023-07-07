e6d7bed19cd60c0fbf3ba845ebf933d7f188dea7
unknown <trahy032@gmail.com>
2023-07-07 15:04:41 +0930

    initial css and javascript commit

---
+++ assets/assets/questions.js
@@ -0,0 +1,59 @@
+// defining set of questions
+var questions = [
+    {
+        question: "Is JavaScript case-sensitive?",
+        choices: ["1. Yes", "2. No"],
+        answer: "1. Yes"
+    },
+
+    {
+        question: "Inside which HTML elemend do we put the JavaScript?",
+        choices: ["1. <js>", "2. <script>", "3. <scripting>", "4. <javascript>"],
+        answer: "2. <script>"
+    },
+    {
+        question: "Where is the correct place to inser a Javascript?",
+        choices: ["1. The <head> section", "2. The <body> section", "3. Both the <head> and <body> section", "4. The <footer> section"],
+        answer: "3. Both the <head> and <body> section"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js?'",
+        choices: ["1. <script href='xxx.js'>", "2. <script name='xxx.js'>", "3. <script src='xxx.js'>", "4. <script type='xxx.js'>"],
+        answer: "3. <script src='xxx.js'>"
+    },
+    {
+        question: "how do you write 'Hello World' in an alert box?",
+        choices: ["1. alertBox('Hello World');", "2. msgBox('Hello World');", "3. msg('Hello World');", "4. alert('Hello World');"],
+        answer: "4. alert('Hello World');"
+    },
+    {
+        question: "How do you create a fucntion in JavaScript?",
+        choices: ["1. function myFunction()", "2. function = myFunction()", "3. function:myFunction()", "4. create myFunction()"],
+        answer: "1. function myFunction()"
+    },
+    {
+        question: "How can you add a comment in JavaScript?",
+        choices: ["1. <!--This is a comment-->", "2. 'This is a comment'", "3. //This is a comment", "4. /*This is a comment*/"],
+        answer: "3. //This is a comment"
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        choices: ["1. var colors = ['red','green','blue']", "2. var colors = 'red','green','blue'", "3. var colors = (1:'red',2:'green',3:'blue')", "4. var colors = 1=('red'),2=('green'),3=('blue')"],
+        answer: "1. var colors = ['red','green','blue']"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element",
+        choices: ["1. onmouseclick", "2. onclick", "3. onmouseover", "4. onchange>"],
+        answer: "2. onclick"
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        choices: ["1. v carName;", "2. var carName;", "3. vrb carName;", "4. variable carName;>"],
+        answer: "2. var carName;"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        choices: ["1. -", "2. x", "3. *", "4. ="],
+        answer: "4. ="
+    },
+]
