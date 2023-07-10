19150e8154ebd92af04b55f5567286e54e9f9694
unknown <trahy032@gmail.com>
2023-07-10 23:13:07 +0930

    fixed bug in highscore.js

--- assets/highscore.js
+++ assets/highscore.js
@@ -4,18 +4,20 @@ var backBtn = document.querySelector("#back-btn");
 var clearBtn = document.querySelector("#clear-btn");
 
 
-// Retrieve local stroage 
+// retrieve local stroage 
 var savedScores = localStorage.getItem("savedScores");
 savedScores = JSON.parse(savedScores);
 
-// sorts stored scores from highest to lowest
-savedScores.sort(function (a, b) {
-    return parseFloat(b.finalscore) - parseFloat(a.finalscore);
-});
 
-// appends score to the initials
+// setting stored items into list 
 if (savedScores !== null) {
 
+    // sorts stored scores from highest to lowest
+    savedScores.sort(function (a, b) {
+        return parseFloat(b.finalscore) - parseFloat(a.finalscore);
+    });
+
+    // appends score to the initials
     for (var i = 0; i < savedScores.length; i++) {
 
         var scoreLi = document.createElement("li");
@@ -24,12 +26,12 @@ if (savedScores !== null) {
     }
 }
 
-// Event listener to move to index page
+// event listener to move to index page
 backBtn.addEventListener("click", function () {
     location.replace("../index.html");
 });
 
-// Event listener to clear scores 
+// event listener to clear scores 
 clearBtn.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
